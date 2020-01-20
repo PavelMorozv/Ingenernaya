caf03c1695c99e78c908a008d9e4af9a5c82978f
PavelMorozv <60074881+PavelMorozv@users.noreply.github.com>
2020-01-21 01:42:19 +0700

    Создание в проекте собственной библиотеки классов.
    добавление функции решающей поставленную задачу

---
+++ PavelMorozov/PMath.cs
@@ -0,0 +1,18 @@
+﻿namespace PavelMorozov
+{
+    public static class PMath
+    {
+        private static int MinMove(int n, int minus, int k)
+        {
+            int kolMove = 0;
+            if (n >= 0 && k >= 2 && minus >= 1)
+                while (n > 0)
+                {
+                    if (n % k == 0) n = n / k; else n = n - 1;
+                    kolMove++;
+                }
+            else return -1;
+            return kolMove;
+        }
+    }
+}
