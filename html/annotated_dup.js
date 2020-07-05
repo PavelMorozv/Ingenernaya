272612f30f34bd52e707b3a51f3992a673f8affb
PavelMorozv <60074881+PavelMorozv@users.noreply.github.com>
2020-07-05 13:01:20 +0700

    Подробная документация для программы

---
+++ html/annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "PavelMorozov", "namespace_pavel_morozov.html", "namespace_pavel_morozov" ],
+    [ "PrakticheskayaRabota3", "namespace_prakticheskaya_rabota3.html", "namespace_prakticheskaya_rabota3" ],
+    [ "UnitTest", "namespace_unit_test.html", "namespace_unit_test" ],
+    [ "Form", "class_form.html", null ]
+];
