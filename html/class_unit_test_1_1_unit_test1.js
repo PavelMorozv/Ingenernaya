90e8f7e4dac0ddfea3dbb47e3fc29803e7fb6224
PavelMorozv <60074881+PavelMorozv@users.noreply.github.com>
2020-07-05 14:28:37 +0700

    Add files via upload

---
+++ html/class_unit_test_1_1_unit_test1.js
@@ -0,0 +1,11 @@
+var class_unit_test_1_1_unit_test1 =
+[
+    [ "TestMinMove_N100_K11_R2_O9", "class_unit_test_1_1_unit_test1.html#a929d06a45dc454f51b1367d7efc8ba5c", null ],
+    [ "TestMinMove_N21_K3_R5_O0", "class_unit_test_1_1_unit_test1.html#a2d9997d5d8da60dc802a276c6836e3d4", null ],
+    [ "TestMinMove_N21_K4_R4_O4", "class_unit_test_1_1_unit_test1.html#aa8e83f094719c4a5958493f606173550", null ],
+    [ "TestMinMove_N22_K3_R6_O0", "class_unit_test_1_1_unit_test1.html#ae40b7f3a854d920174ec8b6baa34059f", null ],
+    [ "TestMinMove_N22_K4_R6_O0", "class_unit_test_1_1_unit_test1.html#ae2271e00aec2aade175cfc5eba9e545c", null ],
+    [ "TestMinMove_N23_K4_R6_O0", "class_unit_test_1_1_unit_test1.html#adb450d9b85dd4fccac2752257c1329fd", null ],
+    [ "TestMinMove_N49_K12_R3_O3", "class_unit_test_1_1_unit_test1.html#aa652793d642dd40ac80cd9bf1df50ed4", null ],
+    [ "TestMinMove_N85_K7_R9_O5", "class_unit_test_1_1_unit_test1.html#aaf5ed8b1d209a68d758e3491122aaded", null ]
+];
